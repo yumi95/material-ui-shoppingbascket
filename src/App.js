10bd3464e9385e8bf42683c010b316a85efd48c1
_yumi_ <yuhey0411@naver.com>
2021-04-27 14:06:42 +0900

    asdf

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Grid } from '@material-ui/core';
+import ShoppingBascket from "./ShoppingBascket";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container direction="column">
+      <Grid item container>
+        <Grid item xs={0} sm={2}/>
+        <Grid item xs={12} sm={8}>
+          <ShoppingBascket />
+
+        </Grid>
+        <Grid item xs={0} sm={2}/>
+      </Grid>
+    </Grid>
   );
 }
 
---
+++ src/ShoppingBascket.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default function ShoppingBascket(){
+    return (
+        <div>
+        <Typography variant="h4" component="h4" align="center">
+            Vision Markets
+        </Typography>
+        </div>
+    );
+}
